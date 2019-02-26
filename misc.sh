13613739504beb4e9fd8755533981840a107d73a
eran7789 <eran7789@gmail.com>
2019-02-26 15:24:44 +0200

    added global npms

--- misc.sh
+++ misc.sh
@@ -3,3 +3,8 @@
 git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
 ~/.scm_breeze/install.sh
 source ~/.bashrc
+
+
+npm install -g nodemon
+npm install -g sequelize-cli
+npm install -g react-native-cli
